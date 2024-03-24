c520a9fdfc0b571d6b2c51661c2a713c024df9ab
ChamChiCham@Laptop <kyjp0623@gmail.com>
2024-03-24 14:47:04 +0900

    #03 Overlapped I/O n:1 echo server

--- client_basic/main.cpp
+++ client_basic/main.cpp
@@ -2,78 +2,65 @@
 #include <WS2tcpip.h>
 #pragma comment (lib, "WS2_32.LIB")
 
-constexpr short PORT = 4'000;
-constexpr char ADDR[] = "127.0.0.1";
+constexpr short PORT = 4000;
+constexpr char SERVER_ADDR[] = "127.0.0.1";
 constexpr int BUFSIZE = 256;
 
-void print_error(const char* msg, int err_no)
+bool bshutdown = false;
+SOCKET server_s;
+WSABUF wsabuf[1];
+char buf[BUFSIZE];
+WSAOVERLAPPED wsaover;
+void CALLBACK send_callback(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+
+void read_n_send()
 {
-	WCHAR* msg_buf;
-	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, err_no,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), reinterpret_cast<LPWSTR>(&msg_buf), 0, NULL);
-	std::cout << msg;
-	std::wcout << L"에러: " << msg_buf << std::endl;
-	while (true);
-	LocalFree(msg_buf);
+	std::cout << "Enter Message : ";
+	std::cin.getline(buf, BUFSIZE);
+	wsabuf[0].buf = buf;
+	wsabuf[0].len = static_cast<int>(strlen(buf)) + 1;
+	if (wsabuf[0].len == 1) {
+		bshutdown = true;
+		return;
+	}
+	ZeroMemory(&wsaover, sizeof(wsaover));
+	WSASend(server_s, wsabuf, 1, nullptr, 0, &wsaover, send_callback);
+}
+
+void CALLBACK recv_callback(DWORD err, DWORD recv_size,
+	LPWSAOVERLAPPED pwsaover, DWORD sendflag)
+{
+	for (DWORD i = 0; i < recv_size; ++i)
+		std::cout << buf[i];
+	std::cout << std::endl;
+	read_n_send();
+}
+
+void CALLBACK send_callback(DWORD err, DWORD sent_size,
+	LPWSAOVERLAPPED pwsaover, DWORD sendflag)
+{
+	wsabuf[0].len = BUFSIZE;
+	DWORD recv_flag = 0;
+	ZeroMemory(pwsaover, sizeof(*pwsaover));
+	WSARecv(server_s, wsabuf, 1, nullptr, &recv_flag, pwsaover, recv_callback);
 }
 
 int main()
 {
-	// Level 0: lnit
-	// 한글 설정
 	std::wcout.imbue(std::locale("korean"));
-
-	// Winsock 초기화
-	WSADATA wsadata;
-	WSAStartup(MAKEWORD(2, 0), &wsadata);
-
-	// Level 1:	Socket
-	// 소켓 생성
-	SOCKET server_s{ WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, 0) };
-
-	// Level 2:	Connect
-	// 서버 주소 설정
+	WSADATA WSAData;
+	WSAStartup(MAKEWORD(2, 0), &WSAData);
+	server_s = WSASocket(AF_INET, SOCK_STREAM,
+		IPPROTO_TCP, nullptr, 0, WSA_FLAG_OVERLAPPED);
 	SOCKADDR_IN server_a;
 	server_a.sin_family = AF_INET;
 	server_a.sin_port = htons(PORT);
-	inet_pton(AF_INET, ADDR, &server_a.sin_addr);
+	inet_pton(AF_INET, SERVER_ADDR, &server_a.sin_addr);
 	connect(server_s, reinterpret_cast<sockaddr*>(&server_a), sizeof(server_a));
-
-
-	// Level 3:	Send/Recv
-	// 메시지 전송
-	while (true) {
-
-		// Send
-		char buf[BUFSIZE];
-		std::cout << "Enter Message: " << std::endl;
-		std::cin.getline(buf, BUFSIZE);
-		WSABUF wsabuf[1];
-		wsabuf[0].buf = buf;
-		wsabuf[0].len = static_cast<int>(strlen(buf) + 1);
-		if (wsabuf[0].len == 1) {
-			break;
-		}
-		DWORD sent_size;
-		WSASend(server_s, &wsabuf[0], 1, &sent_size, 0, 0, 0);
-
-		// Recv
-		wsabuf[0].buf = buf;
-		wsabuf[0].len = static_cast<int>(BUFSIZE);
-		DWORD recv_size;
-		DWORD recv_flag{ 0 };
-		int res = WSARecv(server_s, &wsabuf[0], 1, &recv_size, &recv_flag, nullptr, nullptr);
-		if (0 != res) {
-			print_error("WSARecv", WSAGetLastError());
-		}
-		for (DWORD i{ 0 }; i < recv_size; ++i) {
-			std::cout << buf[i];
-		}
-		std::cout << std::endl;
-		
+	read_n_send();
+	while (false == bshutdown) {
+		SleepEx(0, TRUE);
 	}
-	// Level 4:	Close
-	// 소켓 닫기
 	closesocket(server_s);
 	WSACleanup();
 }
--- server_basic/main.cpp
+++ server_basic/main.cpp
@@ -1,131 +1,133 @@
-//#include <iostream>
-//#include <WS2tcpip.h>
-//using namespace std;
-//#pragma comment (lib, "WS2_32.LIB")
-//const short SERVER_PORT = 4000;
-//const int BUFSIZE = 256;
-//
-//int main()
-//{
-//	// Level 0: init
-//	// Winsock 초기화
-//	WSADATA WSAData;
-//	WSAStartup(MAKEWORD(2, 0), &WSAData);
-//	
-//	// Level 1: Socket
-//	// 소켓 생성
-//	SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0);
-//
-//	// Level 2: Bind
-//	// 바인딩
-//	SOCKADDR_IN server_addr;
-//	ZeroMemory(&server_addr, sizeof(server_addr));
-//	server_addr.sin_family = AF_INET;
-//	server_addr.sin_port = htons(SERVER_PORT);
-//	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-//	bind(s_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
-//
-//	// Level 3: Listen
-//	// 대기열 설정
-//	listen(s_socket, SOMAXCONN);
-//
-//	// Level 4: Accept
-//	// 클라이언트 연결 대기
-//	INT addr_size = sizeof(server_addr);
-//	SOCKET c_socket = WSAAccept(s_socket, reinterpret_cast<sockaddr*>(&server_addr), &addr_size, 0, 0);
-//	
-//	// Level 5: Recv/Send
-//	// 클라이언트와의 통신
-//	for (;;) {
-//
-//		// 클라이언트로부터 메시지 받기
-//		char recv_buf[BUFSIZE];
-//		WSABUF mybuf;
-//		mybuf.buf = recv_buf;  mybuf.len = BUFSIZE;
-//		DWORD recv_byte;
-//		DWORD recv_flag = 0;
-//		WSARecv(c_socket, &mybuf, 1, &recv_byte, &recv_flag, 0, 0);
-//
-//		// 클라이언트로부터 받은 메시지 출력
-//		cout << "Client Sent [" << recv_byte << "bytes] : " << recv_buf << endl;
-//
-//		// 클라이언트에게 메시지 보내기
-//		DWORD sent_byte;
-//		mybuf.len = recv_byte;
-//		WSASend(c_socket, &mybuf, 1, &sent_byte, 0, 0, 0);
-//	}
-//
-//	// Level 6: Close
-//	// 소켓 닫기
-//	WSACleanup();
-//}
-
 #include <iostream>
 #include <WS2tcpip.h>
+#include <unordered_map>
 #pragma comment (lib, "WS2_32.LIB")
 
-constexpr short PORT = 4'000;
-constexpr char ADDR[] = "127.0.0.1";
+constexpr short PORT = 4000;
 constexpr int BUFSIZE = 256;
+bool b_shutdown = false;
+std::unordered_map<LPWSAOVERLAPPED, int> g_session_map;
+
+void CALLBACK send_callback(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+void CALLBACK recv_callback(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+void print_error(const char* msg, int err_no);
+
+class SESSION {
+	char buf[BUFSIZE];
+	WSABUF wsabuf[1];
+	SOCKET client_s;
+	WSAOVERLAPPED over;
+public:
+	SESSION(SOCKET s, int my_id) : client_s(s) {
+		g_session_map[&over] = my_id;
+	}
+	SESSION() {
+		std::cout << "ERROR";
+		exit(-1);
+	}
+	~SESSION() { closesocket(client_s); }
+	void do_recv()
+	{
+		wsabuf[0].buf = buf;
+		wsabuf[0].len = BUFSIZE;
+		DWORD recv_flag = 0;
+		ZeroMemory(&over, sizeof(over));
+		int res = WSARecv(client_s, wsabuf, 1, nullptr, &recv_flag, &over, recv_callback);
+		if (0 != res) {
+			int err_no = WSAGetLastError();
+			if (WSA_IO_PENDING != err_no)
+				print_error("WSARecv", WSAGetLastError());
+		}
+	}
+
+	void do_send(int recv_size)
+	{
+		wsabuf[0].len = recv_size;
+		int res = WSASend(client_s, wsabuf, 1, nullptr, 0, &over, send_callback);
+		if (0 != res) {
+			print_error("WSARecv", WSAGetLastError());
+		}
+	}
+
+	void print_message(DWORD recv_size)
+	{
+		int my_id = g_session_map[&over];
+		std::cout << "Client[" << my_id << "] Sent : ";
+		for (DWORD i = 0; i < recv_size; ++i)
+			std::cout << buf[i];
+		std::cout << std::endl;
+	}
+};
+
+std::unordered_map<int, SESSION> g_players;
 
 void print_error(const char* msg, int err_no)
 {
 	WCHAR* msg_buf;
-	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, err_no,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), reinterpret_cast<LPWSTR>(&msg_buf), 0, NULL);
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, err_no,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		reinterpret_cast<LPWSTR>(&msg_buf), 0, NULL);
 	std::cout << msg;
-	std::wcout << L"에러: " << msg_buf << std::endl;
+	std::wcout << L" : 에러 : " << msg_buf;
 	while (true);
 	LocalFree(msg_buf);
 }
 
+void CALLBACK send_callback(DWORD err, DWORD sent_size,
+	LPWSAOVERLAPPED pover, DWORD recv_flag)
+{
+	if (0 != err) {
+		print_error("WSASend", WSAGetLastError());
+	}
+	g_players[g_session_map[pover]].do_recv();
+}
+
+void CALLBACK recv_callback(DWORD err, DWORD recv_size,
+	LPWSAOVERLAPPED pover, DWORD recv_flag)
+{
+	if (0 != err) {
+		print_error("WSARecv", WSAGetLastError());
+	}
+	int my_id = g_session_map[pover];
+	if (0 == recv_size) {
+		g_players.erase(my_id);
+		return;
+	}
+
+	g_players[my_id].print_message(recv_size);
+	g_players[my_id].do_send(recv_size);
+}
+
 int main()
 {
+	// setting
 	std::wcout.imbue(std::locale("korean"));
+	WSADATA WSAData;
+	WSAStartup(MAKEWORD(2, 0), &WSAData);
 
-	WSADATA wsadata;
-	WSAStartup(MAKEWORD(2, 0), &wsadata);
+	// create socket
+	SOCKET server_s = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, WSA_FLAG_OVERLAPPED);
 
-	SOCKET server_s{ WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, 0, 0) };
-
+	// bind socket address
 	SOCKADDR_IN server_a;
 	server_a.sin_family = AF_INET;
 	server_a.sin_port = htons(PORT);
 	server_a.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-
 	bind(server_s, reinterpret_cast<sockaddr*>(&server_a), sizeof(server_a));
 
+	// listen
 	listen(server_s, SOMAXCONN);
-
 	int addr_size = sizeof(server_a);
-	SOCKET client_s = WSAAccept(server_s, reinterpret_cast<sockaddr*>(&server_a), &addr_size, nullptr, 0);
-	while (true) {
+	int id = 0;
 
-		char buf[BUFSIZE];
-		WSABUF wsabuf[1];
-		wsabuf[0].buf = buf;
-		wsabuf[0].len = static_cast<int>(BUFSIZE);
-		DWORD recv_size;
-		DWORD recv_flag{ 0 };
-		int res = WSARecv(client_s, &wsabuf[0], 1, &recv_size, &recv_flag, nullptr, nullptr);
-		if (0 != res) {
-			print_error("WSARecv", WSAGetLastError());
-		}
-		if (wsabuf[0].len == 1) {
-			break;
-		}
-
-		for (DWORD i{ 0 }; i < recv_size; ++i) {
-			std::cout << buf[i];
-		}
-		std::cout << std::endl;
-
-		wsabuf[0].buf = buf;
-		wsabuf[0].len = static_cast<int>(strlen(buf) + 1);
-		DWORD sent_size;
-		WSASend(client_s, &wsabuf[0], 1, &sent_size, 0, 0, 0);
-
+	while (false == b_shutdown) {
+		// accept (overlapped i/o)
+		SOCKET client_s = WSAAccept(server_s, reinterpret_cast<sockaddr*>(&server_a), &addr_size, nullptr, 0);
+		g_players.try_emplace(id, client_s, id);
+		g_players[id++].do_recv();
 	}
+	g_players.clear();
 	closesocket(server_s);
 	WSACleanup();
 }
